8dcc8b8c36b80de31de59ddd5f9af27cded930f6
sg313 <sachin914597@gmail.com>
2024-07-30 13:02:30 +0530

    app changes

--- server/app.js
+++ server/app.js
@@ -1,35 +1,41 @@
 import dotenv from 'dotenv';
-import express, { Router } from 'express'
-import connectDB from './src/databases/db.js'
+import express from 'express';
+import connectDB from './src/databases/db.js';
 import routers from './src/routes/routes.js';
-import cors from 'cors'
-const app = express()
-dotenv.config()
+import cors from 'cors';
 
+dotenv.config();
+
+const app = express();
 const port = process.env.PORT;
+
 const corsConfig = {
-    origin: ['http://localhost:3000'], // Allow requests from this origin
+    origin: 'http://localhost:3000', // Allow requests from this origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-}
+};
+
 connectDB();
-app.use(express.json())
-app.options("",cors(corsConfig))
+
+// Middleware to handle JSON request bodies
+app.use(express.json());
+
+// CORS configuration
 app.use(cors(corsConfig));
 
+// Handle preflight requests for all routes
+app.options('*', cors(corsConfig));
+
+// Test route to check if the server is running
 app.use('/test', (req, res) => {
-    res.send("Hello Server IS working 7:)")
-})
-app.use('/api', routers)
+    res.send("Hello Server IS working 8:)");
+});
 
+// API routes
+app.use('/api', routers);
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-
-
-
-
-
-
-
+    console.log(`Server is running on port ${port}`);
+});
